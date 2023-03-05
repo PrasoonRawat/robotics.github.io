4a8ee852082bf4401a2f02e2f0525170fdd40f97
prasoon <prasoon818@gmail.com>
2023-03-05 20:05:22 +0530

    i don't know

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const slider = new A11YSlider(document.querySelector(".slider"), {
   adaptiveHeight: false,
   dots: true,
   centerMode: true,
-  arrows: true,
+  arrows: false,
   responsive: {
     480: {
       dots: false, // dots enabled 1280px and up
